554238590367e1ad0b8f999b261a1fde4484e16f
Ravi <ravindrak1474@gmail.com>
2023-04-10 10:43:34 +0530

    initial commit

---
+++ FS/Html/index.js
@@ -0,0 +1,59 @@
+// // // console.log("Hello Word");
+// // // var number1=34;
+// // // var number2=56;
+// // // console.log(number1+number2);
+
+// // // var marks={
+// // //     ravi:34,
+// // //     shubham:78,
+// // //     mahesh:99
+// // // }
+// // // console.log(marks)
+
+// // // var a=100;
+// // // var b=10;
+// // // console.log("The value of a+b is",a+b);
+// // // console.log("The value of a-b is",a-b);
+// // // console.log("The value of a*b is",a*b);
+// // // console.log("The value of a/b is",a/b);
+
+
+// // // comparision operator
+// // // var x=34;
+// // // var y=56;
+// // // console.log(x==y)
+// // // console.log(x>=y)
+// // // console.log(x<=y)
+// // // console.log(x>y)
+// // // console.log(x<y)
+
+
+// // // logical operator
+
+// // // console.log(true&&true)
+// // function avg(a,b)
+// // {
+// //     return(a+b)/2;
+// // }
+// // c1=avg(4,6);
+// // c2=avg(14,16);
+// // console.log(c1,c2,c1,c1);
+
+
+// var age=14;
+// if(age>18)
+// {
+//     console.log('you can drink rasna water');
+    
+// }else
+//     {
+//         console.log('you cannot drink rasna water');
+
+//     }
+
+var arr=[1,2,3,4,15,6,7,8,9];
+console.log(arr);
+for(var i=0;i<arr.length;i++)
+{
+    console.log(arr[i])
+}
